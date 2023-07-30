6671f861d6df864c89d7ea2f043253486a3a4877
Makari0us <dfu2277@gmail.com>
2023-07-30 13:33:16 -0700

    7/30 sunday

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -20,30 +20,7 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   final picker = ImagePicker();
   File? _profileImage;
-
-  Future<void> _pickImage() async {
-    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
-    if (pickedImage != null) {
-      setState(() {
-        _profileImage = File(pickedImage.path);
-        // Save the new image to Firebase Storage
-      });
-    }
-  }
-
-  Future<UserProfile> _fetchUserData() async {
-    final userRef =
-        FirebaseFirestore.instance.collection('Users').doc(widget.userID);
-    final userData = await userRef.get();
-
-    if (userData.exists) {
-      return UserProfile.fromMap(
-          userData.data() as Map<String, dynamic>, userData.id);
-    } else {
-      return UserProfile(
-          id: widget.userID, name: '', age: null, swimTeam: null);
-    }
-  }
+  bool _isUploadingImage = false;
 
   @override
   Widget build(BuildContext context) {
@@ -51,9 +28,23 @@ class _ProfilePageState extends State<ProfilePage> {
       future: _fetchUserData(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Profile Page'),
+            ),
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
         } else if (snapshot.hasError) {
-          return Text('Error: $snapshot.error}');
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Profile Page'),
+            ),
+            body: Center(
+              child: Text('Error: ${snapshot.error}'),
+            ),
+          );
         } else {
           final user = snapshot.data;
           return Scaffold(
@@ -66,72 +57,24 @@ class _ProfilePageState extends State<ProfilePage> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   InkWell(
-                    onTap: _pickImage,
+                    onTap: () => _pickImage(),
                     child: CircleAvatar(
                       radius: 70,
                       backgroundImage: _profileImage != null
                           ? FileImage(_profileImage!)
                           : AssetImage(
-                                  'assets/images/default_profile_image.png')
-                              as ImageProvider<Object>,
+                              'assets/images/default_profile_image.png'),
                     ),
                   ),
                   SizedBox(height: 30.0),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Column(
-                        children: [
-                          Text(
-                            'Name',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 5.0),
-                          Text('${user!.name}',
-                              style: TextStyle(
-                                fontSize: 18.0,
-                              )),
-                        ],
-                      ),
+                      _buildInfoColumn('Name', '${user!.name}'),
                       SizedBox(width: 50.0),
-                      Column(
-                        children: [
-                          Text(
-                            'Age',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 5.0),
-                          Text('${user!.age.toString()}',
-                              style: TextStyle(
-                                fontSize: 18.0,
-                              )),
-                        ],
-                      ),
+                      _buildInfoColumn('Age', '${user.age ?? "N/A"}'),
                       SizedBox(width: 50.0),
-                      Column(
-                        children: [
-                          Text(
-                            'Swim Team',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 5.0),
-                          Text(
-                            '${user!.swimTeam}',
-                            style: TextStyle(
-                              fontSize: 18.0,
-                            ),
-                          ),
-                        ],
-                      ),
+                      _buildInfoColumn('Swim Team', '${user.swimTeam ?? "N/A"}'),
                     ],
                   ),
                 ],
@@ -141,5 +84,64 @@ class _ProfilePageState extends State<ProfilePage> {
         }
       },
     );
+  }
+
+  Widget _buildInfoColumn(String title, String value) {
+    return Column(
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 5.0),
+        Text(
+          value,
+          style: TextStyle(
+            fontSize: 18.0,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _pickImage() async {
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _isUploadingImage = true;
+      });
+
+      final imageFile = File(pickedImage.path);
+      // Save the new image to Firebase Storage
+      // Implement the upload logic here and update _profileImage once the upload is complete
+
+      setState(() {
+        _profileImage = imageFile;
+        _isUploadingImage = false;
+      });
+    }
+  }
+
+  Future<UserProfile> _fetchUserData() async {
+    final userRef =
+        FirebaseFirestore.instance.collection('Users').doc(widget.userID);
+    final userData = await userRef.get();
+
+    if (userData.exists) {
+      return UserProfile.fromMap(
+        userData.data() as Map<String, dynamic>,
+        userData.id,
+      );
+    } else {
+      return UserProfile(
+        id: widget.userID,
+        name: '',
+        age: null,
+        swimTeam: null,
+      );
+    }
   }
 }
